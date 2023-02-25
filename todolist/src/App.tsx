bbb97d3dc55e0aaef4d32304350c6e7531859511
Belozerov Egor <belaz.egor@gmail.com>
2023-02-25 13:13:02 +0300

    2-filter+button+state

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,28 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {TaskType, Todolist} from "./component/Todolist";
+
+export type FilterValueType ="all" | "completed" | "active"
+
 
 function App() {
+
+
+
+    let [tasks, setTasks] = useState<Array<TaskType>>([
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false},
+        {id: 4, title: "Redux", isDone: true}
+    ]);
+    let [filter, setFilter] = useState<FilterValueType>("all")
+
+    function removeTask(id: number) {
+        let filterTask = tasks.filter((t) => {
+           return t.id !== id;
+
+            /* if (t.id !== id) {
+                return true
+            }                       это тоже самое что и выше
+            else
+            {
+                return false
+            };*/
+        })
+        setTasks(filterTask);
+    }
+
+    function changeFilter(value: FilterValueType) {
+        setFilter(value);
+    }
+
+    let tasksForTodolist = tasks;
+    if (filter === "completed") {
+        tasksForTodolist = tasks.filter(t => t.isDone === true);
+    }
+    if (filter === "active") {
+        tasksForTodolist = tasks.filter(t => t.isDone === false);
+    }
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title="What to learn" tasks={tasksForTodolist}
+                      removeTask={removeTask}
+                      changeFilter = {changeFilter}
+            />
+
+
         </div>
-    );
+    )
 }
 
 export default App;
